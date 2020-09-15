f61442e17bc95a9769adef854c06dce9d4833ce6
arsh9806 <arshdeep55singh@gmail.com>
2020-09-15 18:01:43 +0530

    Sidebar added

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import SidebarOptions from "./SidebarOptions";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 import "../Assets/Sidebar.css";
+
 function Sidebar() {
     return (
         <div className="sidebar">
@@ -9,9 +13,12 @@ function Sidebar() {
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
                 alt=""
             />
-            <SidebarOptions title="Home"/>
-            <SidebarOptions title="Search"/>
-            <SidebarOptions title="Your library"/>
+            <SidebarOptions Icon={HomeIcon} title="Home"/>
+            <SidebarOptions Icon={SearchIcon} title="Search"/>
+            <SidebarOptions Icon={LibraryMusicIcon} title="Your library"/>
+            <br/>
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr/>
         </div>
     )
 }
--- src/Components/SidebarOptions.js
+++ src/Components/SidebarOptions.js
@@ -3,7 +3,8 @@ import '../Assets/SidebarOptions.css';
 function SidebarOptions({title, Icon}) {
     return (
         <div className="sidebarOption">
-            <p>{title}</p>
+        {Icon && <Icon className="sidebarOption__icon" />}
+        {Icon? <h4>{title}</h4>: <p>{title}</p> }
         </div>
     )
 }
